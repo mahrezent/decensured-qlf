3a92b723a5ba4eddc81246295ab0a82428ac26a6
mahrezent <107353686+mahrezent@users.noreply.github.com>
2022-06-13 14:39:18 +0200

    Update decensured.meta.js

--- decensured.meta.js
+++ decensured.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        Décensured
+// @name        Décensured QLF
 // @namespace   decensuredjvcom
 // @version     0.2.7
-// @downloadURL https://github.com/Rand0max/decensured/raw/master/decensured.user.js
-// @updateURL   https://github.com/Rand0max/decensured/raw/master/decensured.meta.js
+// @downloadURL https://github.com/mahrezent/decensured-qlf/raw/master/decensured.user.js
+// @updateURL   https://github.com/mahrezent/decensured-qlf/raw/master/decensured.meta.js
 // @author      Rand0max
 // @description Contrer la censure et le 410 sur les forums JVC
-// @icon        https://risibank.fr/cache/medias/0/2/209/20968/thumb.png
+// @icon        https://risibank.fr/cache/medias/0/1/117/11784/thumb.png
 // @match       http://www.jeuxvideo.com/forums/*
 // @match       https://www.jeuxvideo.com/forums/*
 // @connect     myexternalip.com
@@ -23,16 +23,16 @@
 // @grant       GM.listValues
 // @grant       GM.getResourceText
 // @grant       GM.xmlHttpRequest
-// @resource    DECENSURED_CSS https://raw.githubusercontent.com/Rand0max/decensured/master/decensured.css
+// @resource    DECENSURED_CSS https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/decensured.css
 // @require     https://unpkg.com/gm-storage
-// @require     variables.js
-// @require     storage.js
-// @require     helper.js
-// @require     cryptohelper.js
-// @require     jvcinternal.js
-// @require     messages.js
-// @require     topics.js
-// @require     plugins.js
-// @require     main.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/variables.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/storage.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/helper.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/cryptohelper.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/jvcinternal.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/messages.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/topics.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/plugins.js
+// @require     https://raw.githubusercontent.com/mahrezent/decensured-qlf/master/main.js
 // @run-at      document-end
 // ==/UserScript==
